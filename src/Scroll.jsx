70d981af6497fec8672e2dcc79784c9da87565c3
hajhouj <alaa.spread@gmail.com>
2026-09-14 18:24:08 +0100

    changement dans le projet

--- src/Scroll.jsx
+++ src/Scroll.jsx
@@ -1,17 +1,16 @@
-import { useScroll } from 'framer-motion'
-import React, {useEffect} from 'react'
+import { useScroll, useTransform } from 'framer-motion'
+import React, {useEffect, useRef, useState} from 'react'
 import { motion } from 'framer-motion';
 
 
 function Scroll() {
-  const resultat=useScroll();
-   
+   const ref1=useRef()
+   const scroll1=useScroll({target:ref1,offset: ["start 50%", "end 50%"]});
+   const scale = useTransform(scroll1.scrollYProgress, [0,1], [0.3,1]);
 
-  return (
-       <motion.div style={{width:"100%",height:"20px",scaleX:resultat.scrollYProgress,background:"red",position:"fixed",top:0,right:0}}>
+   return <motion.div ref={ref1} style={{border:"1px solid black",width:"900px",height:"900px",scale:scale}}>
 
-       </motion.div>
-  )
+   </motion.div>
 }
 
 export default Scroll
