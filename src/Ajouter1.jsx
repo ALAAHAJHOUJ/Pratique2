fd9088cf5c26bc756cd14119a7e3d49a3651747e
hajhouj <alaa.spread@gmail.com>
2026-09-21 20:10:58 +0000

    changement

--- src/Ajouter1.jsx
+++ src/Ajouter1.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Ajouter1({ref1,ref2,Envoyer,changer,supprimer1}) {
+function Ajouter1() {
 
-    
+
+  const [state1,setState1]=useState({})
+  const [afficher,setAfficher]=useState(false)
+
+  const Envoyer=()=>{
+
+        fetch("http://localhost:4000/tester888/",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({name1:"hajhouj",name2:"israe"})})
+        .then((res)=>{console.log(res);return res.json()})
+        .then((res)=>{console.log(res);setState1(res);setAfficher(true)})
+        .catch((error)=>{console.log(error)}) 
+  
+    }
+  
+
   return (
-        <div className='border-black border-[1px] flex flex-col justify-center items-center gap-[10px] w-[400px] h-[400px] flex-wrap'>
-              <input ref={ref1} type="text" className='border-black border-[1px] w-[170px] h-[50px] rounded-[10px]' />
-              <input ref={ref2} type="text" className='border-black border-[1px] w-[170px] h-[50px] rounded-[10px]'  />
-              <button onClick={Envoyer} className='bg-blue-500 text-white rounded-[10px] p-4'>Ajouter</button>
-              <button onClick={changer} className='bg-green-400 text-white rounded-[10px] p-4'>Recuperer</button>
-              <button onClick={supprimer1} className='bg-red-500 text-white rounded-[10px] p-4'>Supprimer</button>
-         </div>
+        <div className='border-black border-[1px] text-center flex flex-col justify-center items-center gap-[10px] w-[400px] h-[400px] flex-wrap'>
+         
+         {
+              afficher==true?`le propr1 est:${state1.name1} et la propr2 est:${state1.name2} et la propr3 est:${state1.propr}`:''
+         }
+
+        </div>
   )
 }
 
--- src/Router1.jsx
+++ src/Router1.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import API2 from './API2';
 import Supprimer22 from './Supprimer22';
 import Recuperer11 from './recuperer11';
+import Ajouter1 from './Ajouter1';
 
 
 
@@ -17,6 +18,7 @@ function Router1() {
                 <Route path='/Ajouter/' element={<API2></API2>}></Route>
                 <Route path='/supprimer/' element={<Supprimer22></Supprimer22>}></Route>
                 <Route path='/recuperer/' element={<Recuperer11></Recuperer11>}></Route>
+                <Route path='/tester/' element={<Ajouter1></Ajouter1>}></Route>
                 <Route path='*' element={<h2 className='text-[40px] font-bold'>page introuvable</h2>}></Route>
       </Routes>
     </BrowserRouter>
